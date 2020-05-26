67fcffafad74f75e5e63de869f855b49b744d5e7
Bennjo96 <59307362+Bennjo96@users.noreply.github.com>
2020-05-26 19:39:38 +0200

    Update CoffeeMachine.java

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,7 +1,6 @@
 // Project : Redi`s member Secured Coffee Machine ;
 // Programmed by : Beny
-// Function : Provide 4 types of coffee and used only by Redi members  **secured** used only with code.(password)
-// HINT: cheat sheet for coffee types available and free to every Redi Member;
+
 
 
 import java.util.*;
@@ -19,7 +18,8 @@ class CoffeeMachine {
         b = input.next().charAt(0);
 
         if (b == 'Y' || b == 'y') {
-            IngredientsProcessing ingredients = new IngredientsProcessing();     //Instances For Ingredients
+             //Instances For Ingredients
+            IngredientsProcessing ingredients = new IngredientsProcessing();    
             ingredients.StartMachine();
 
             //In order to call All Private Method...
